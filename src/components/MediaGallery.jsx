0ec3cd56b3b1481e10e70ab461a1c2de01a14e2f
jacob <aoqnwnd@naver.com>
2025-11-10 19:34:49 +0900

    [fix] 이미지 비율 재수정

--- src/components/MediaGallery.jsx
+++ src/components/MediaGallery.jsx
@@ -5,13 +5,19 @@ function MediaGallery({ items }) {
   return (
     <div className="media-gallery">
       {items.map((item) => {
+        if (item.type === 'image') {
+          return (
+            <section key={item.id} className="media-section">
+              <img src={item.src} alt={item.alt} className="media-section__image" />
+            </section>
+          );
+        }
+
         return (
           <section key={item.id} className="media-section">
-            {item.type === 'image' ? (
-              <img src={item.src} alt={item.alt} className="media-section__image" />
-            ) : (
+            <div className="media-section__video">
               <YouTubeEmbed title={item.title} embedUrl={item.src} />
-            )}
+            </div>
           </section>
         );
       })}
--- src/components/YouTubeEmbed.jsx
+++ src/components/YouTubeEmbed.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 function YouTubeEmbed({ title, embedUrl }) {
   return (
-    <div className="media-section__video">
-      <iframe
-        title={title}
-        src={embedUrl}
-        loading="lazy"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      />
-    </div>
+    <iframe
+      title={title}
+      src={embedUrl}
+      loading="lazy"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowFullScreen
+    />
   );
 }
 
